a64636fd19fe5d25e352a6e2385c8e2fec01269d
amal <amalthomas0444@gmail.com>
2026-04-18 14:24:30 +0530

    cors fixed

--- app/main.py
+++ app/main.py
@@ -79,7 +79,7 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://admin.roadozcourier.com" # production
+    "https://admin.roadozcourier.com"
 ]
 
 app.add_middleware(
