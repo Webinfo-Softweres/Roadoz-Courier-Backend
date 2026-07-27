295deea2afa140cce8dfbc34b10af5680b62dae2
knowscope80 <EMAIL_USED_IN_KNOWSCOPE80>
2026-07-27 15:38:00 +0530

    tripsheet changessssssss

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -32,3 +32,4 @@ from app.models import warehouse
 from app.models import webconfiguration
 from app.models import month_end_closing
 from app.models.trip_sheet import TripSheet, TripSheetOrder
+from app.models.order_intransit import OrderInTransit
